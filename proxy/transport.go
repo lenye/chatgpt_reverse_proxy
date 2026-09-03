5c0ecad13718194e152020fdc8051977582aac5c
lenye <pyweb@qq.com>
2026-09-03 23:28:14 +0800

    fix: http transport

--- proxy/transport.go
+++ proxy/transport.go
@@ -9,20 +9,20 @@ import (
 var httpTransport http.RoundTripper = &http.Transport{
 	Proxy: http.ProxyFromEnvironment,
 	DialContext: (&net.Dialer{
-		Timeout:   5 * time.Second,
+		Timeout:   30 * time.Second,
 		KeepAlive: 30 * time.Second,
 	}).DialContext,
 
-	IdleConnTimeout: 30 * time.Second,
+	MaxIdleConns:    100,
+	IdleConnTimeout: 90 * time.Second,
 
-	MaxIdleConns:        100,
-	MaxIdleConnsPerHost: 20,
+	ResponseHeaderTimeout: 10 * time.Minute,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
 
-	TLSHandshakeTimeout:    5 * time.Second,
-	ResponseHeaderTimeout:  10 * time.Second,
-	ExpectContinueTimeout:  1 * time.Second,
 	WriteBufferSize:        32 * 1024,
 	ReadBufferSize:         32 * 1024,
 	MaxResponseHeaderBytes: 32 * 1024,
-	ForceAttemptHTTP2:      true,
+
+	ForceAttemptHTTP2: true,
 }
